3a7bed9f36fcafd8b51299d2c61a4bd36aab7998
Phil Gyford <phil@gyford.com>
2013-05-14 15:46:26 +0100

    Maybe get some event data. Doubt it works.

--- publication.rb
+++ publication.rb
@@ -9,7 +9,7 @@ end
 
 
 helpers do
-  def oauth_client
+  def consumer
     OAuth2::Client.new(ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'],
       {
         :site => 'https://api.github.com',
@@ -26,16 +26,14 @@ get '/' do
 end
 
 
-get '/edition/' do
-  erb :publication
-end
-
-
+# The user has just come here from BERG Cloud to authenticate with Github.
 get '/configure/' do
+  # Save these for use when the user returns.
   session[:bergcloud_return_url] = params['return_url']
   session[:bergcloud_error_url] = params['error_url']
 
-  url = oauth_client.auth_code.authorize_url(
+  # Send them to Github to approve us.
+  url = consumer.auth_code.authorize_url(
     :redirect_uri => url('/configure/return/'),
     :scope => 'repo:status' # Also use notifications?
   )
@@ -43,11 +41,16 @@ get '/configure/' do
 end
 
 
-# Must be a subdirectory of the calling URL.
-# This URL is specified in the Github application.
+# The user has returned here from approving us (or not) at Github.
+# URL be a subdirectory of the calling URL.
+# This URL is also specified in the Github application.
 get '/configure/return/' do
+  # If there's no code returned, something went wrong, or the user declined
+  # to authenticate us. This is the least nasty thing we can do right now:
+  redirect session[:bergcloud_error_url] if !params[:code]
+
   begin
-    access_token = oauth_client.auth_code.get_token(
+    access_token = consumer.auth_code.get_token(
                                   params[:code], :redirect_uri => url('/configure/return'))
     query = URI.encode_www_form('config[access_token]' => access_token.token)
     redirect session[:bergcloud_return_url] + '?' + query 
@@ -55,6 +58,17 @@ get '/configure/return/' do
     puts "OAuth error: #{$!}"
     redirect session[:bergcloud_error_url]
   end
+end
+
+
+get '/edition/' do
+  request = OAuth2::AccessToken.new(consumer, params[:access_token]) 
+  response = request.get('/users/:user/received_events').body
+  # etag Digest::MD5.hexdigest(params[:access_token] + Date.today.strftime('%d%m%Y'))
+  # Testing, always changing etag:
+  etag Digest::MD5.hexdigest(params[:access_token] + Time.now.strftime('%M%H-%d%m%Y'))
+  # erb :publication
+  response
 end
 
 
